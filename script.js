2d113ea2615f81cc0338d6a08ebba39245cb17f2
Atreya Bag <atreyabag26@gmail.com>
2021-10-09 22:15:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let taskbar = document.getElementsByClassName("taskbar")[0]
 let startmenu = document.getElementsByClassName("startmenu")[0]
 
-taskbar.addEventListener("onclick", ()=>{
+taskbar.addEventListener("click", ()=>{
     console.log("clicked");
     if(startmenu.style.bottom == "50px"){
         startmenu.style.bottom = "-655px"
